86a9a8b58f3b9c09c0f5f0232dd2012b95a01d33
Youning Xia <youning.xia@gmail.com>
2023-03-01 00:33:21 +0000

    Implement intersect function

--- shapes/Sphere.cpp
+++ shapes/Sphere.cpp
@@ -8,23 +8,42 @@
 namespace rt
 {
 
-
 	// Destructor
-	Sphere::~Sphere() {}
+	Sphere::~Sphere()
+	{ /*TODO*/
+	}
 
 	/**
-	 * Computes whether a ray hit the specific instance of a sphere shape and returns the hit data
+	 * @brief Computes whether a ray hit the specific instance of
+	 * a sphere shape and returns the hit data
 	 *
 	 * @param ray cast ray to check for intersection with shape
-	 *
-	 * @return hit struct containing intersection information
-	 *
+	 * @return Hit struct containing intersection information
 	 */
 	Hit Sphere::intersect(Ray ray)
 	{
 
 		Hit h;
-		//-----------to be implemented -------------
+		// TODO
+
+		/* t^2 * b⋅b+2tb⋅(O−C)+(O−C)⋅(O−C)−r^2=0 */
+		Vec3f oc = ray.orig - center;
+		float a = ray.dir.dotProduct(ray.dir);
+		float b = 2.0 * oc.dotProduct(ray.dir);
+		float c = oc.dotProduct(oc) - radius * radius;
+		float discriminant = b * b - 4.0 * a * c;
+
+		if (discriminant < 0)
+		{
+			h.point = ray.orig - 1.0 * ray.dir;
+			h.intersect = false;
+		}
+		else
+		{
+			float t = (-b - sqrt(discriminant)) / (2.0 * a);
+			h.point = ray.orig - t * ray.dir;
+			h.intersect = true;
+		}
 
 		return h;
 	}
