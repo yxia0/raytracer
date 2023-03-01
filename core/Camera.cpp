6c56c2c2ab5a36ffa43f15b606cd619beed81e15
Youning Xia <youning.xia@gmail.com>
2023-03-01 00:30:33 +0000

    Format & add default constructor setting & add getRay interface

--- core/Camera.cpp
+++ core/Camera.cpp
@@ -8,45 +8,53 @@
 #include "cameras/Pinhole.h"
 #include "cameras/ThinLens.h"
 
-namespace rt{
+namespace rt
+{
+
+	Camera::Camera(int height, int width, int fov) : height(height), width(width), fov(fov)
+	{
+		position = Vec3f(0.0, 0.0, 0.0);
+		lookat = Vec3f(0.0, 0.0, -0.1);
+		up = Vec3f(0.0, 1.0, 0.0);
+	};
 
 	Camera::~Camera(){};
 
+	/**
+	 * Factory function that returns camera subclass based on camera specifications
+	 *
+	 * @param cameraSpecs camera specifications json object
+	 *
+	 * @return camera subclass instance
+	 *
+	 */
+	Camera *Camera::createCamera(Value &cameraSpecs)
+	{
 
+		// check if cameratype is defined
 
-/**
- * Factory function that returns camera subclass based on camera specifications
- *
- * @param cameraSpecs camera specifications json object
- *
- * @return camera subclass instance
- *
- */
-Camera* Camera::createCamera(Value& cameraSpecs){
+		if (!cameraSpecs.HasMember("type"))
+		{
+			std::cerr << "Camera type not specified" << std::endl;
+			exit(-1);
+		}
 
-	//check if cameratype is defined
+		std::string cameraType = cameraSpecs["type"].GetString();
 
-	if (!cameraSpecs.HasMember("type")){
-		std::cerr<<"Camera type not specified"<<std::endl;
-		exit(-1);
+		// return camera object based on camera specs
+		// TODO: parse postion, lookat, up
+		if (cameraType.compare("pinhole") == 0)
+		{
+			return new Pinhole(cameraSpecs["width"].GetInt(),
+							   cameraSpecs["height"].GetInt(),
+							   cameraSpecs["fov"].GetInt());
+		}
+		else if (cameraType.compare("thinlens") == 0)
+		{
+			return new ThinLens();
+		}
+
+		return 0;
 	}
 
-	std::string cameraType=cameraSpecs["type"].GetString();
-
-	//return camera object based on camera specs
-	if (cameraType.compare("pinhole")==0){
-		return new Pinhole(cameraSpecs["width"].GetInt(),
-				cameraSpecs["height"].GetInt(),
-				cameraSpecs["fov"].GetInt());
-
-	}else if (cameraType.compare("thinlens")==0){
-		return new ThinLens();
-	}
-
-	return 0;
-
-}
-
-
-
-} //namespace rt
+} // namespace rt
--- core/Camera.h
+++ core/Camera.h
@@ -7,91 +7,73 @@
 #define CAMERA_H_
 
 #include "rapidjson/document.h"
-
+#include "math/geometry.h"
+#include "core/RayHitStructs.h"
 
 using namespace rapidjson;
 
-namespace rt{
+namespace rt
+{
 
-class Camera{
-public:
+	class Camera
+	{
+	public:
+		//
+		// Constructors
+		//
+		Camera(){};
+		Camera(int height, int width, int fov);
+		//
+		// Destructor
+		//
+		virtual ~Camera();
 
-	//
-	// Constructors
-	//
-	Camera(){};
-	Camera(int height, int width, int fov):height(height), width(width), fov(fov){};
+		//
+		// factory function : returns camera instance dynamically based on camera specifications
+		//
+		static Camera *createCamera(Value &cameraSpecs);
 
-	//
-	// Destructor
-	//
-	virtual ~Camera();
+		//
+		// print function (to be implemented by the subclasses )
+		//
+		virtual void printCamera() = 0;
 
+		//
+		// other camera functions (to complete)
+		//
 
-	//
-	// factory function : returns camera instance dynamically based on camera specifications
-	//
-	static Camera* createCamera(Value& cameraSpecs);
+		virtual Ray getRay(int hIndex, int wIndex) = 0;
 
+		//
+		// Getters and Setters
+		//
+		int getHeight() const { return height; }
 
-	//
-	// print function (to be implemented by the subclasses )
-	//
-	virtual void printCamera()=0;
+		void setHeight(int height) { this->height = height; }
 
+		int getWidth() const { return width; }
 
+		void setWidth(int width) { this->width = width; }
 
-	//
-	// other camera functions (to complete)
-	//
+		void setPosition(Vec3f position) { this->position = position; }
 
+		void setLookAt(Vec3f lookat) { this->lookat = lookat; }
 
+		void setUp(Vec3f up) { this->up = up; }
 
+	protected:
+		//
+		// camera members
+		//
+		int height;
+		int width;
+		int fov; // field of view
 
+		Vec3f position;
+		Vec3f lookat;
+		Vec3f up;
+	};
 
-
-
-
-
-
-
-
-
-
-	//
-	// Getters and Setters
-	//
-	int getHeight() const {
-		return height;
-	}
-
-	void setHeight(int height) {
-		this->height = height;
-	}
-
-	int getWidth() const {
-		return width;
-	}
-
-	void setWidth(int width) {
-		this->width = width;
-	}
-
-
-
-protected:
-
-	//
-	//camera members
-	//
-	int height;
-	int width;
-	int fov; //field of view
-
-};
-
-} //namespace rt
-
-
+} // namespace rt
 
 #endif /* CAMERA_H_ */
