87a4b95501a6bb5d6bc3e7b468df75d67dc7b4e8
Youning Xia <youning.xia@gmail.com>
2023-03-15 21:54:44 +0000

    Add pointlights objects

--- lights/PointLight.h
+++ lights/PointLight.h
@@ -8,20 +8,28 @@
 #define POINTLIGHT_H_
 
 #include "core/LightSource.h"
+#include "math/geometry.h"
 
+namespace rt
+{
 
-namespace rt{
+    class PointLight : public LightSource
+    {
 
-class PointLight: public LightSource{
+    public:
+        PointLight(){};
+        PointLight(Vec3f position, Vec3f is, Vec3f id) : position(position), is(is), id(id){};
 
+        Vec3f getPos() const { return position; }
+        Vec3f getIs() const { return is; }
+        Vec3f getId() const { return id; }
 
-};
+    private:
+        Vec3f position;
+        Vec3f is;
+        Vec3f id;
+    };
 
-
-
-} //namespace rt
-
-
-
+} // namespace rt
 
 #endif /* POINTLIGHT_H_ */
