52aeb7264afe3d44304a9813bcf92959b1420ef8
Youning Xia <youning.xia@gmail.com>
2023-04-22 17:01:47 +0100

    Add ray hit structure

--- core/RayHitStructs.h
+++ core/RayHitStructs.h
@@ -6,6 +6,7 @@
 #define CORE_RAYHITSTRUCTS_H_
 
 #include "math/geometry.h"
+#include "core/Material.h"
 
 namespace rt
 {
@@ -29,13 +30,16 @@ namespace rt
 		Vec3f orig; /* ray origin */
 		Vec3f dir;	/* ray direction - unit vector */
 		Vec3f pix_center;
+		bool can_refract;
 	};
 
 	struct Hit
 	{
 		double t;
-		Vec3f point;  // point where ray hits a shape
-		Vec3f normal; // surface normal of hit point
+		Vec3f point;		// point where ray hits a shape
+		Vec3f normal;		// surface normal of hit point
+		bool enter_object;	// if the ray is entering the object or leaving
+		Material *material; // pointer to material object
 	};
 }
 
